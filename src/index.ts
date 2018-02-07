630fc080bf4a8d0b1420f35bf739a64ff6cba14e
Brad Gardner <bradgardner@sevenhillstechnology.com>
2018-02-07 08:43:47 -0500

    incremented version and removed console log in test

--- src/index.ts
+++ src/index.ts
@@ -58,7 +58,7 @@ export class Server {
             .filter((fileName : any) => fileName.indexOf('.') !== 0 && fileName.slice(-3) === '.js')
             .forEach((fileName : any) => {
                 this.server.route(require(path.join(directory, fileName)).default);
-                console.log(`Added ${fileName} to the API routes.`);
+                //console.log(`Added ${fileName} to the API routes.`);
             });
     }
 
