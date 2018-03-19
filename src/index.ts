0793f7cd762304b61d10841480dac7d269fbf061
ZakFahey <z.fahey@gmail.com>
2018-03-19 09:26:28 -0400

    Fixed cors issue with POST requests (#4)

--- src/index.ts
+++ src/index.ts
@@ -149,6 +149,7 @@ export class Server {
                     response.type('text/plain');
                     response.header('Access-Control-Allow-Origin', '*');
                     response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+                    response.header('Access-Control-Allow-Headers', 'Content-Type');
                     return response;
                 },
                 config: {
