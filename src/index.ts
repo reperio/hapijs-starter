976346d9887b87c19ce191079d70d5aa957c5cfe
Spencer Dangel <dangelspencer@gmail.com>
2018-03-20 18:28:18 -0400

    Add auth header to Access-Control-Allow-Headers list

--- src/index.ts
+++ src/index.ts
@@ -150,7 +150,7 @@ export class Server {
                     response.type('text/plain');
                     response.header('Access-Control-Allow-Origin', '*');
                     response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-                    response.header('Access-Control-Allow-Headers', 'Content-Type');
+                    response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
                     return response;
                 },
                 config: {
